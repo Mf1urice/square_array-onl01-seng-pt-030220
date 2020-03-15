7dbc78e45a67b833b13fecf6dd98fa8cd896f1e6
Maurice Falls <falls8019oct@gmail.com>
2020-03-15 03:58:18 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,12 @@
 def square_array(array)
   # your code here
+  array = [] array.each do |array|
+  numbers = [1,2,3]
+ 
+square_array(numbers)
+# => [1,4,9])
+ 
+new_numbers = [9,10,16,25]
+ 
+square_array(new_numbers)
 end
